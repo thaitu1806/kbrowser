cbf3348423b7f73103f545c689b09b3e4a94bcd4
An Tan <thainv.bp@gmail.com>
2026-04-27 17:01:31 +0700

    feat: add custom icon support for Electron window and portable exe

--- src/main/index.ts
+++ src/main/index.ts
@@ -15,6 +15,7 @@ function createWindow(): void {
     height: 800,
     minWidth: 960,
     minHeight: 600,
+    icon: path.join(__dirname, '..', '..', '..', 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
